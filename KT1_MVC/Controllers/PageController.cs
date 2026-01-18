8689a43eca765cb077772873e6cfe79b7bcb47d4
savi0810 <alex08max2007@gmail.com>
2026-01-18 17:41:04 +0300

    Добавьте файлы проекта.

---
+++ KT1_MVC/Controllers/PageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KT1_MVC.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        public IActionResult Greet(string name)
+        {
+            ViewData["name"] = name ?? "Alex";
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string message)
+        {
+            ViewData["message"] = message;
+            return View("EditRes");
+        }
+
+    }
+}
---
+++ KT1_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KT1_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
